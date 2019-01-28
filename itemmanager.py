bcb8e0f9b791528123f5816f9ba004cda64a7f53
TAEHUN\taehun <kithsme@gmail.com>
2019-01-29 07:34:34 +0900

    add method for finding consumed item and attribute for it

--- itemmanager.py
+++ itemmanager.py
@@ -1,5 +1,13 @@
+import pandas as pd
+
 class ItemManager:
     
+    def __init__(self):
+        self.consumed_item = {}
+        
+    def clear_consumed_item(self):
+        self.consumed_item = {}
+
     def get_WH_shelf_node_list(self, wh):        
         snodes = {}
         for s in wh.WH_shelf_node_list:
@@ -30,8 +38,27 @@ class ItemManager:
     def use_item(self, item, useFrom):
         
         # check availability fisrt!
+        self.consumed_item[useFrom] = item
         use_item = self.shelves[useFrom].index(item)
         del self.shelves[useFrom][use_item]
         
         use_from = self.items[item].index(useFrom)
         del self.items[item][use_from]
+
+
+class ItemAttribute:
+    
+    """ Temporal for Demo """
+    def __init__(self):
+        # temporal
+        
+        item = ['desk_1', 'desk_2', 'desk_3', 
+                'table_1','table_2','table_3',
+                'chair_1','chair_2','chair_3',
+                'footrest']
+        vol = [10, 10, 10,
+               15, 15, 15,
+               3, 3, 3,
+               3]
+        
+        self.ItemAttb = pd.DataFrame.from_dict({'ITEM':item,'VOL':vol})
