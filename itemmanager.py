9c28ed4a009bf9b8310c41e8f68cd06410abd983
TAEHUN\taehun <kithsme@gmail.com>
2019-02-06 16:34:26 +0900

    refactoring itemmanager  - move generate test case item into ittemmanager.py

--- itemmanager.py
+++ itemmanager.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import random
+import collections
+import pickle
 
 class ItemManager:
     
@@ -62,3 +65,58 @@ class ItemAttribute:
                3]
         
         self.ItemAttb = pd.DataFrame.from_dict({'ITEM':item,'VOL':vol})
+
+def gen_test_items(orders, wh, name):
+
+    im = ItemManager()
+
+    im.get_WH_shelf_node_list(wh)
+
+    full_list = ['table_1', 'table_2', 'table_3',
+                 'desk_1', 'desk_2', 'desk_3',
+                 'chair_1', 'chair_2', 'chair_3',
+                 'footrest']
+
+    im.get_item_full_list(full_list)
+
+    xx = [0, 2, 3, 5, 6, 8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 23, 24, 26, 27]
+    yy = [i for i in range(2, 24)] + [i for i in range(25, 46)]
+
+    all_items = []
+
+    for o in orders:
+        all_items = all_items + o.items
+
+    ic = collections.Counter(all_items)
+
+    unoccupied = []
+    for x in xx:
+        for y in yy:
+            unoccupied.append((x, y))
+
+
+    mult = len(unoccupied) / sum(ic.values())
+
+    if mult < 1:
+
+        "************************REPLENISH ITEMS FIRST!!!!!!!!!!!!!!*********************"
+    else:
+        if mult * 0.8 > 1:
+            mult = mult * 0.8
+        else:
+            mult = mult
+
+    for each_item in full_list:
+
+        for i in range(int(ic[each_item] * mult)):
+            random.shuffle(unoccupied)
+            x = unoccupied[0][0]
+            y = unoccupied[0][1]
+            im.replenish_item(each_item, 'SHELF_({},{})'.format(x, y))
+
+            unoccupied = unoccupied[1:]
+
+        print(each_item, '\nOrdered: ', ic[each_item], '\nReplenished: ', len(im.items[each_item]))
+
+    with open('./' + name, 'wb') as f:
+        pickle.dump(im, f)
