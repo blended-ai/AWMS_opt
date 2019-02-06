a17c7133427e385392ca855590b9edc3315c3428
TAEHUN\taehun <kithsme@gmail.com>
2019-02-07 01:54:08 +0900

    refactoring workermanager.py  - add generate test case workermanager  - add threshold for find route candidate  - add attributes for plan stats

--- workermanager.py
+++ workermanager.py
@@ -7,6 +7,23 @@ class Worker:
         self.on_from = on_from
         self.travel = 0
         self.job_done = 0
+        self.last_job_done_at = 0
+        self.idle = 0
+        self.wait = 0
+        self.initial_pos = worker_pos
+        self.initial_status = worker_status
+        self.initial_on_from = on_from
+
+    def reset(self):
+
+        self.pos = self.initial_pos
+        self.status = self.initial_status
+        self.on_from = self.initial_on_from
+
+        self.idle = 0
+        self.wait = 0
+        self.travel = 0
+        self.job_done = 0
         
         
 
@@ -19,8 +36,8 @@ class WorkerManager:
     def set_worker(self, worker):
         self.workers[worker.id] = worker
         
-    def get_next_on(self):
-        
+    def get_next_on(self, threshold=50):
+
         minOn = 1000000
         who = None
         
@@ -28,11 +45,31 @@ class WorkerManager:
             
             if self.workers[w].on_from < minOn:
                 minOn = self.workers[w].on_from
-                who = [w]
-            elif self.workers[w].on_from == minOn:
-                who = who + [w]
-        
-        return minOn, who
+
+        ret = []
+        for w in self.workers:
+
+            if minOn <= self.workers[w].on_from <= minOn + threshold:
+                ret.append((w, self.workers[w].on_from))
+
+        return ret
+
+    def reset_workers(self):
+
+        for w in self.workers:        
+            self.workers[w].reset()
+    
+
+def gen_test_workerManager():
+    wm = WorkerManager()
+
+    wm.set_worker(Worker("WK_1", "DOOR_(1,0)", 1))
+    wm.set_worker(Worker("WK_2", "DOOR_(7,0)", 1))
+    wm.set_worker(Worker("WK_3", "DOOR_(13,0)", 1))
+    wm.set_worker(Worker("WK_4", "DOOR_(19,0)", 1))
+    wm.set_worker(Worker("WK_5", "DOOR_(25,0)", 1))
+
+    return wm
 
 
 if __name__ == "__main__":
